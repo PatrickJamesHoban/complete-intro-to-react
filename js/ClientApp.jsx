eb70335457d5d7e2aec7f650de7bca6404fd4cb1
PatrickJamesHoban <Patrick.J.Hoban@gmail.com>
2018-09-19 19:23:18 -0500

    updated index and clientapp files for minor typing errors

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,40 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-// const ce = React.createElement;
+// arrow functions have an implicit return.
+// const Add = (a, b) => {
+//   return a + b;
+// };
+// const Add2 = (a, b) => a + b;
 
-const MyTitle = function(props) {
-  // return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
-
-  // above now rewritten in jsx as below return.
-  // use parenthesis after return to let JS know it needs to go to the next line.
-  return (
-    <div>
-      <h1 style={{ color: props.color }}> {props.title} </h1>
+const App = () => (
+  // can't use class in JSX as that is a reserved word for JavaScript.
+  <div className="app">
+    <div className="landing">
+      <h1>SVideo</h1>
+      <input type="text" placeholder="Search" />
+      <a>or Browse All</a>
     </div>
-  );
-};
+  </div>
+);
 
-const MyFirstComponent = function() {
-  // return ce(
-  //   'div',
-  //   { id: 'my-first-component' },
-  // ce(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen' }),
-  // ce(MyTitle, { title: 'Stranger Things', color: 'GreenYellow' }),
-  // ce(MyTitle, { title: 'Rick & Morty', color: 'LimeGreen' }),
-  // ce(MyTitle, { title: 'Silicon Valley', color: 'Lime' })
-
-  // without parenthesis after return, div needs to be on the same line.
-  return (
-    <div id="my-first-component">
-      // trailing slash must be present in JSX
-      <MyTitle title="Game of Thrones" color="YellowGreen" />
-      <MyTitle title="Stranger Things" color="GreenYellow" />
-      <MyTitle title="Rick & Morty" color="LimeGreen" />
-      <MyTitle title="Silicon Valley" color="Lime" />
-    </div>
-  );
-};
-
-// render(ce(MyFirstComponent), document.getElementById('app'));
-render(<MyFirstComponent/> document.getElementById('app'));
+render(<App />, document.getElementById('app'));
