1e5366ea4a94bdac938c327ad15d53bfb655ca85
PatrickJamesHoban <Patrick.J.Hoban@gmail.com>
2018-10-01 18:06:42 -0500

    Updated webpack config to use browser router and updated clientapp with a switch and 404 error routing

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { HashRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Landing from './Landing';
 import Search from './Search';
 
@@ -10,16 +10,23 @@ import Search from './Search';
 // };
 // const Add2 = (a, b) => a + b;
 
+const FourOhFour = () => <h1>404</h1>;
+
 const App = () => (
   // HashRouter is a higher order component. Introduces behavior, routing, but doesn't actually render markup itself.
   // one route of our application. // exact = means that path must match exactly. // component = what to render if path matches.
   // cant use class in JSX as that is a reserved word for JavaScript.
-  <HashRouter>
+  // component FourOhFour => If nothing else matches, render this...
+  <BrowserRouter>
     <div className="app">
-      <Route exact path="/" component={Landing} />
-      <Route path="/search" component={Search} />
+      {/* Switch will go in priority order, dishes out the first one matched. */}
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route path="/search" component={Search} />
+        <Route component={FourOhFour} />
+      </Switch>
     </div>
-  </HashRouter>
+  </BrowserRouter>
 );
 
 render(<App />, document.getElementById('app'));
