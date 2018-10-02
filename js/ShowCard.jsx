02020e4c88bb20e1d5a86a3609be9f5099cb3a1e
PatrickJamesHoban <Patrick.J.Hoban@gmail.com>
2018-10-02 17:24:36 -0500

    Cleaned up typo after h3

--- js/ShowCard.jsx
+++ js/ShowCard.jsx
@@ -7,7 +7,7 @@ const ShowCard = props => (
     {/* alt with backticks is another way to show string concatenation */}
     <div>
       <h3>{props.show.title}</h3>
-      <h4>({props.show.year})</h4>{' '}
+      <h4>({props.show.year})</h4>
       {/* parentheses are literally part of the string output to the dom in this instance */}
       <p>{props.show.description}</p>
     </div>
