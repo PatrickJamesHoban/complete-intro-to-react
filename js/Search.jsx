db5ea6f808ba4f58b908b6efd261cf80af3d03a5
PatrickJamesHoban <Patrick.J.Hoban@gmail.com>
2018-10-02 16:35:06 -0500

    Updated search component to import preload of data file and verify that json info was loading to the dom as expected

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
+// below is a reference to the data.json file
+import preload from '../data.json';
 
-const Search = () => <h1> hi this is default search </h1>;
+const Search = () => (
+  <div className="search">
+    {/* Debugging trick to see JSON output. */}
+    <pre>
+      <code>{JSON.stringify(preload, null, 4)}</code>
+    </pre>
+  </div>
+);
 
 export default Search;
