ae03fc37a93e48c0e7cd54dfaf8a37ebeffd257a
PatrickJamesHoban <Patrick.J.Hoban@gmail.com>
2018-10-02 17:21:14 -0500

    Update show list in search to provide more complete show card info. Create new ShowCard file and move content from Search file into ShowCard component.

--- js/Search.jsx
+++ js/Search.jsx
@@ -5,7 +5,7 @@ import preload from '../data.json';
 const Search = () => (
   <div className="search">
     {preload.shows.map(show => (
-      <h3>{show.title}</h3>
+
     ))}
   </div>
 );
---
+++ js/ShowCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// props becomes whatever the parent passes down into show card.
+const ShowCard = props => (
+  <div className="show-card">
+    <img alt={`${props.show.title} Show Poster`} src={`/public/img/posters/${props.show.poster}`} />
+    {/* alt with backticks is another way to show string concatenation */}
+    <div>
+      <h3>{props.show.title}</h3>
+      <h4>({props.show.year})</h4>{' '}
+      {/* parentheses are literally part of the string output to the dom in this instance */}
+      <p>{props.show.description}</p>
+    </div>
+  </div>
+);
+
+export default ShowCard;
