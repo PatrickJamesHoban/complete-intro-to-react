41987a1ae0aed148860086f03d6f83dfd67e9961
PatrickJamesHoban <Patrick.J.Hoban@gmail.com>
2018-10-09 19:06:04 -0500

    Added import showcard call to search jsx file and rendered show element

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import ShowCard from './ShowCard';
 // below is a reference to the data.json file
 import preload from '../data.json';
 
 const Search = () => (
   <div className="search">
     {preload.shows.map(show => (
-
+      <ShowCard show={show} />
     ))}
   </div>
 );
