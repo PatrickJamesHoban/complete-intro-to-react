66dc20ec4e6e00ea427fa7486248d7974c8c14fd
PatrickJamesHoban <Patrick.J.Hoban@gmail.com>
2018-10-12 17:47:57 -0500

    Added key property to search jsx file

--- js/Search.jsx
+++ js/Search.jsx
@@ -3,10 +3,11 @@ import ShowCard from './ShowCard';
 // below is a reference to the data.json file
 import preload from '../data.json';
 
+// key= is used to keep react from blowing away everything every time something changes, example a sort function for shows on a page.  Provide a unique identifier that Search can then keep track of.
 const Search = () => (
   <div className="search">
     {preload.shows.map(show => (
-      <ShowCard {...show} show={show} />
+      <ShowCard key={show.imdbID} {...show} />
     ))}
   </div>
 );
