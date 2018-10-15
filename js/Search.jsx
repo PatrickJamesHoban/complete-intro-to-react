b4ff0a9889f47d65595675d08c28ddb162beba10
PatrickJamesHoban <Patrick.J.Hoban@gmail.com>
2018-10-15 18:21:07 -0500

    Added a header with search bar to searched item page and changed search to an es6 class component

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ShowCard from './ShowCard';
 // below is a reference to the data.json file
 import preload from '../data.json';
 
 // key= is used to keep react from blowing away everything every time something changes, example a sort function for shows on a page.  Provide a unique identifier that Search can then keep track of.
-const Search = () => (
-  <div className="search">
-    <div>
-      {preload.shows.map(show => (
-        <ShowCard key={show.imdbID} {...show} />
-      ))}
-    </div>
-  </div>
-);
+class Search extends Component {
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>svideo</h1>
+          <input type="text" placeholder="search" />
+        </header>
+        <div>
+          {preload.shows.map(show => (
+            <ShowCard key={show.imdbID} {...show} />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Search;
