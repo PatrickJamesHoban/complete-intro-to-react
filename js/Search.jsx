e5c529014675145ea76f23d5d2fd48942581594a
PatrickJamesHoban <Patrick.J.Hoban@gmail.com>
2018-10-15 17:44:04 -0500

    Add styled component Wrapper to ShowCard jsx file and replace div with Wrapper component. Add second Image component and replace img tag with Image.

--- js/Search.jsx
+++ js/Search.jsx
@@ -6,9 +6,11 @@ import preload from '../data.json';
 // key= is used to keep react from blowing away everything every time something changes, example a sort function for shows on a page.  Provide a unique identifier that Search can then keep track of.
 const Search = () => (
   <div className="search">
-    {preload.shows.map(show => (
-      <ShowCard key={show.imdbID} {...show} />
-    ))}
+    <div>
+      {preload.shows.map(show => (
+        <ShowCard key={show.imdbID} {...show} />
+      ))}
+    </div>
   </div>
 );
 
--- js/ShowCard.jsx
+++ js/ShowCard.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
-
 // Note, prop-types no longer exists in react library
 import { string } from 'prop-types';
+// styled is another react library
+import styled from 'styled-components';
+
+// tagged template literal double back-ticks `` pure javascript
+// write as you would normal CSS within
+const Wrapper = styled.div`
+  width: 32%;
+  border: 2px solid #333;
+  border-radius: 4px;
+  margin-bottom: 15px;
+  padding-right: 10px;
+  overflow: hidden;
+`;
+
+const Image = styled.img`
+  width: 46%;
+  float: left;
+  margin-right: 10px;
+`;
 
 // props becomes whatever the parent passes down into show card.
 const ShowCard = props => (
-  <div className="show-card">
-    <img alt={`${props.title} Show Poster`} src={`/public/img/posters/${props.poster}`} />
+  <Wrapper>
+    <Image alt={`${props.title} Show Poster`} src={`/public/img/posters/${props.poster}`} />
     {/* alt with backticks is another way to show string concatenation */}
     <div>
       <h3>{props.title}</h3>
@@ -14,7 +32,7 @@ const ShowCard = props => (
       {/* parentheses are literally part of the string output to the dom in this instance */}
       <p>{props.description}</p>
     </div>
-  </div>
+  </Wrapper>
 );
 
 // => shape is the object, poster, title, etc. are the properties of that object
