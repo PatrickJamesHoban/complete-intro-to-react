b03ddd3de73ebe817b9d3f527475e39290dedf52
PatrickJamesHoban <Patrick.J.Hoban@gmail.com>
2018-10-17 16:40:47 -0500

    Added plugin to babelrc file. Added parser babel eslint to eslintrc file. Replace constructor props function with state in search and update handleSearchTermChange to an arrow function.

--- js/Search.jsx
+++ js/Search.jsx
@@ -5,19 +5,14 @@ import preload from '../data.json';
 
 // key= is used to keep react from blowing away everything every time something changes, example a sort function for shows on a page.  Provide a unique identifier that Search can then keep track of.
 class Search extends Component {
-  // Constructor takes in props and passes props up. Simple boilerplate.
-  constructor(props) {
-    super(props);
-    this.state = {
-      searchTerm: 'some sort of debug statement'
-    };
-    // put bind function inside constructor function so that it is only called once.
-    this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
-  }
+  // must use 'state' as the variable in this instance when replacing older constructor(props) method.
+  state = {
+    searchTerm: 'some debug statement'
+  };
   // .setState lets react know that it has something that it needs to re-render.  Don't modify state directly.
-  handleSearchTermChange(event) {
+  handleSearchTermChange = event => {
     this.setState({ searchTerm: event.target.value });
-  }
+  };
   render() {
     return (
       <div className="search">
