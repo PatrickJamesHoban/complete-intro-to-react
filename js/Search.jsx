77a34c06ded9e7e143032ac0bbcaa4a4bab9bf6d
PatrickJamesHoban <Patrick.J.Hoban@gmail.com>
2018-10-17 16:06:18 -0500

    Create state and provide a change handler for the search box in the header.

--- js/Search.jsx
+++ js/Search.jsx
@@ -5,12 +5,30 @@ import preload from '../data.json';
 
 // key= is used to keep react from blowing away everything every time something changes, example a sort function for shows on a page.  Provide a unique identifier that Search can then keep track of.
 class Search extends Component {
+  // Constructor takes in props and passes props up. Simple boilerplate.
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: 'some sort of debug statement'
+    };
+    // put bind function inside constructor function so that it is only called once.
+    this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
+  }
+  // .setState lets react know that it has something that it needs to re-render.  Don't modify state directly.
+  handleSearchTermChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
   render() {
     return (
       <div className="search">
         <header>
           <h1>svideo</h1>
-          <input type="text" placeholder="search" />
+          <input
+            onChange={this.handleSearchTermChange}
+            value={this.state.searchTerm}
+            type="text"
+            placeholder="search"
+          />
         </header>
         <div>
           {preload.shows.map(show => (
